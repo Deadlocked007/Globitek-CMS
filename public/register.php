7154e2f4960f28f9b5a68f2434b4f56f547cff23
Siraj Zaneer <szaneer@gmail.com>
2017-01-17 05:45:21 -0500

    Initial
    - Basic UI
    - SQL setup

---
+++ public/register.php
@@ -0,0 +1,66 @@
+<?php
+  require_once('../private/initialize.php');
+
+  // Set default values for all variables the page needs.
+
+  // if this is a POST request, process the form
+  // Hint: private/functions.php can help
+
+    // Confirm that POST values are present before accessing them.
+
+    // Perform Validations
+    // Hint: Write these in private/validation_functions.php
+
+    // if there were no errors, submit data to database
+
+      // Write SQL INSERT statement
+      // $sql = "";
+
+      // For INSERT statments, $result is just true/false
+      // $result = db_query($db, $sql);
+      // if($result) {
+      //   db_close($db);
+
+      //   TODO redirect user to success page
+
+      // } else {
+      //   // The SQL INSERT statement failed.
+      //   // Just show the error, not the form
+      //   echo db_error($db);
+      //   db_close($db);
+      //   exit;
+      // }
+
+?>
+
+<?php $page_title = 'Register'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <h1>Register</h1>
+  <p>Register to become a Globitek Partner.</p>
+
+  <?php
+    // TODO: display any form errors here
+    // Hint: private/functions.php can help
+  ?>
+
+  <!-- TODO: HTML form goes here -->
+  <form action="calc.php" method="post">
+	First name:<br>
+	<input type="text" name="fName">
+	<br>
+	Last name:<br>
+	<input type="text" name="lName">
+	<br>
+	Email:<br>
+	<input type="text" name="email">
+	<br>
+	Username:<br>
+	<input type="text" name="uName">
+	<br><br>
+	<input type="submit" name="submit" value="Submit">
+  </form>
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
